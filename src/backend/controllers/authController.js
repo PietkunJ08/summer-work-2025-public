6f6b51dc3558c4c726a6b5038fc413471dcc1c76
PietkunJ08 <24PietkunJ342@collyers.ac.uk>
2025-09-06 15:32:19 +0100

    Update authController.js

--- src/backend/controllers/authController.js
+++ src/backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const db = require("../../config/db");
+const db = require("../db");
 
 exports.register = async (req, res) => {
   try {
