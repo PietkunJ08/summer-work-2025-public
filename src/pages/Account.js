e001f4ddd3479f7abecc80df40f169d4362a2d2d
PietkunJ08 <24PietkunJ342@collyers.ac.uk>
2025-09-07 13:24:28 +0100

    Update Account.js

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
+import { apiUrl } from "../config/api";
 
 const Wrapper = styled.div`
   text-align: center;
@@ -39,7 +38,7 @@ export default function Account() {
 
     (async () => {
         try {
-            const res = await fetch(`${API_BASE}/api/users/me`, {
+            const res = await fetch(apiUrl("/api/users/me"), {
                 headers: { Authorization: `Bearer ${token}` }
             });
 
