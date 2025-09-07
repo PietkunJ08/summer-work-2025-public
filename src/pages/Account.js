acc9cf975f12d4d62641e1ef28d637d7002e3e15
PietkunJ08 <24PietkunJ342@collyers.ac.uk>
2025-09-07 13:11:58 +0100

    Update Account.js

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { apiUrl } from "../config/api";
 
 const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
 
